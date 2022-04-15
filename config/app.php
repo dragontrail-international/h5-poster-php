337b4bf5e4ac1eb0a8dcdb39d717f637155e5399
shrekuu <shrekuu@163.com>
2022-04-15 15:49:40 +0800

    install and config intervention/image

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -192,6 +193,9 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+
+        'Image' => Intervention\Image\Facades\Image::class,
+
     ])->toArray(),
 
 ];
